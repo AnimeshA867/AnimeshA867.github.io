cf7a6b12f19e706088a731bbd3f178a65565b0aa
Animesh Acharya <animeshacharya867+github@gmail.com>
2022-12-09 19:17:31 +0545

    Update form.php

--- form.php
+++ form.php
@@ -8,13 +8,15 @@ $message = $_POST['message'];
 // Validate the data
 if (empty($name) || empty($email) || empty($message)) {
     // If any of the fields are empty, return an error
-    echo "Error: All fields are required.";
+    $result = array('status' => 'error', 'message' => 'All fields are required.');
+    echo json_encode($result);
     exit;
 }
 
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     // If the email address is not valid, return an error
-    echo "Error: Invalid email address.";
+    $result = array('status' => 'error', 'message' => 'Invalid email address.');
+    echo json_encode($result);
     exit;
 }
 
@@ -22,7 +24,7 @@ if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 $to = "animeshacharya867@gmail.com";
 
 // Set the email subject
-$subject = "$_POST['subject']";
+$subject = "New message from $name";
 
 // Build the email message
 $message = "Name: $name\n";
@@ -35,9 +37,11 @@ $headers = "From: $name <$email>";
 // Send the email
 if (mail($to, $subject, $message, $headers)) {
     // If the email was sent successfully, return a success message
-    echo "Success: Your message has been sent.";
+    $result = array('status' => 'success', 'message' => 'Your message has been sent.');
+    echo json_encode($result);
 } else {
     // If there was an error sending the email, return an error message
-    echo "Error: There was a problem sending your message.";
+    $result = array('status' => 'error', 'message' => 'There was a problem sending your message.');
+    echo json_encode($result);
 }
 ?>
